fa3edfdda7d7778d2108f7ee436cf12b9073e1ec
monayemislam <monayemislam@gmail.com>
2023-02-26 08:39:33 -0800

    JWT token installed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,8 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
