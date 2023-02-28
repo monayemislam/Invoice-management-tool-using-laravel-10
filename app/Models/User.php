9d8982eeeb08bce546c9b01a1f5d98bec19dfa53
DESKTOP-9ORVBHL\Monayem Islam Tamal <monayem.dev@gmail.com>
2023-02-28 14:59:29 +0600

    Auth Controller has been completed and JWT token is now properly working

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements MustVerifyEmail,JWTSubject
+class User extends Authenticatable implements MustVerifyEmail, JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -110,7 +110,7 @@ class User extends Authenticatable implements MustVerifyEmail,JWTSubject
     {
         return $this->hasMany(RecurringInvoice::class);
     }
-    
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
